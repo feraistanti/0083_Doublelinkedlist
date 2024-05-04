5899d684ee00a06f599d5be55393b1fe2be92b49
feraistanti <fistanti551@gmail.com>
2024-05-04 10:58:29 +0700

    Prosedur untuk search data

--- 0083_Doublelinkedlist/0083_Doublelinkedlist.cpp
+++ 0083_Doublelinkedlist/0083_Doublelinkedlist.cpp
@@ -134,3 +134,38 @@ bool listEmpty()
 
 void traverse()
 {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else
+	{
+		cout << "\nRecords in ascending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode != NULL)
+		{
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}
+}
+
+void revtraverse()
+{
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else
+	{
+		cout << "\nRecords in descending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
+		while (currentNode != NULL)
+		{
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
+}
+
+void searchData()
+{
